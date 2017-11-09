69faa739143443bdb72fa7ed7352440b76c9cbaa
julien <julienfr112@gmail.com>
2017-11-09 21:12:30 +0100

    add my name

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup, Extension
 setup(
-    name="nxsvg", version="0.1pre",
-    author="Yu Feng",
+    name="nxsvg", version="0.11pre",
+    author="Yu Feng, Julien Fraichard",
     author_email="rainwoodman@gmail.com",
     description="Native SVG rendering of NetworkX Graphs",
     requires=["svgwrite"],
-    package_dir = {'nxsvg': 'src'},
-    packages= ['nxsvg'],
+    package_dir={'nxsvg': 'src'},
+    packages=['nxsvg'],
 )
-
